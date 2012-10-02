6c50b3b54436277b5b3b969765f45bb9b5c07a98
Bin Liu <blayneliu@gmail.com>
2012-10-01 22:45:42 -0700

    add tracking on referer url

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -3,15 +3,24 @@ class UsersController extends AppController {
 	public $helper = array('Html', 'Form');
 
 	public function index() {
-		$this->set('title_for_layout', 'Home');
-		$this->set('ip_addr', $_SERVER['REMOTE_ADDR']);
+		$this -> set('title_for_layout', 'Home');
+		//get remote IP address
+		$this -> set('ip_addr', $_SERVER['REMOTE_ADDR']);
+		//get visit date and time
 		$datetime = new DateTime();
-		$this->set('date_time', $datetime->format('Y/m/d H:i:s'));
+		$this -> set('date_time', $datetime -> format('Y/m/d H:i:s'));
+		//get referer URL
+		if (isset($_SERVER['HTTP_REFERER'])) {
+			$referer_url = $_SERVER['HTTP_REFERER'];
+		} else {
+			$referer_url = NULL;
+		}
 		
-		$this->loadModel('Access_log');
-		$log_data=array('Access_log'=>array('ip_addr'=>$_SERVER['REMOTE_ADDR'],
-											'date_time'=>$datetime->format('Y/m/d H:i:s')));
-		$this->Access_log->save($log_data);
+		$this -> loadModel('Access_log');
+		$log_data = array('Access_log' => array('ip_addr' => $_SERVER['REMOTE_ADDR'], 
+											  'date_time' => $datetime -> format('Y/m/d H:i:s'), 
+											'referer_url' => $referer_url));
+		$this -> Access_log -> save($log_data);
 	}
-	
+
 }
